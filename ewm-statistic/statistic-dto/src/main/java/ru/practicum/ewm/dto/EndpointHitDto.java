f95f05223953dfa501905e0f383668b3b55a74ce
kirshumir01 <kirshumir@yandex.ru>
2024-10-23 05:29:33 +0300

    Исправлены ошибки codestyle (main-svc)

--- ewm-statistic/statistic-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
+++ ewm-statistic/statistic-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
